7ed45bbfa11daa02a3a633e21b6e93b7ad059b57
Olakunle Odegbaro <oodegbaro@gmail.com>
2018-07-18 08:51:03 +0100

    Added the schemas, using migrations

---
+++ database/migrations/2018_07_18_073053_create_employees_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('company_id');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('email')->nullable();
+            $table->string('phone')->nullable();
+            $table->timestamps();
+
+            $table->foreign('company_id')->references('id')->on('companies')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('employees', function(Blueprint $table){
+            $table->dropForeign(['company_id']);
+        });
+        Schema::dropIfExists('employees');
+    }
+}
